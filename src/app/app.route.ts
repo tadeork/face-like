78e016a861895f07751c3a5867f7ae1fa96eaeac
Tadeo Riveros Koloszwa @ bsf <tadeoriverosk@gmail.com>
2017-09-20 16:09:25 -0300

    new routes

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -6,8 +6,9 @@ import {LoginComponent } from './login/login.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [AuthorizeGuard] },
+  { path: 'user', component: HomeComponent, canActivate: [AuthorizeGuard] },
   { path: 'friends', component: FriendsComponent, canActivate: [AuthorizeGuard] },
+  { path: 'home', component: FriendsComponent, canActivate: [AuthorizeGuard] },
   { path: 'login', component: LoginComponent },
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: '**', component: NotFoundComponent },
